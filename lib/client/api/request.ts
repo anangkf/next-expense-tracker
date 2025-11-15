f698a5aa03895850e4fbf2836b4be72cba233982
anangkf <anangkhoirulfadli@gmail.com>
2025-11-15 10:52:23 +0700

    feat: integrated logout functionality with api

--- lib/client/api/request.ts
+++ lib/client/api/request.ts
@@ -1,9 +1,27 @@
 import constant from "@/lib/constant";
 import axios from "axios";
+import { getCookie } from "cookies-next";
 
 const api = axios.create({
   baseURL: `${constant.BASE_URL_API}/api/v1`,
   timeout: 30_000,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = getCookie(constant.TOKEN_KEYNAME as string);
+
+    const authRoutes = ["/auth/login", "/auth/register"];
+
+    if (token && !authRoutes.some((route) => config.url?.includes(route))) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
--- services/shared/useLogout.ts
+++ services/shared/useLogout.ts
@@ -1,11 +1,15 @@
 import api from "@/lib/client/api";
+import constant from "@/lib/constant";
 import { useMutation } from "@tanstack/react-query";
+import { deleteCookie } from "cookies-next";
 
 export const useLogout = () => {
   return useMutation({
     mutationFn: async () => {
       try {
         await api.post("/auth/logout");
+        deleteCookie(constant.TOKEN_KEYNAME as string);
+        deleteCookie(constant.REFRESH_TOKEN_KEYNAME as string);
       } catch (error: any) {
         throw error?.response?.data;
       }
